f0fe7dd67cb38b794302fb1dc03055ef4b963d43
David Simek <david.simek1998@gmail.com>
2022-08-07 10:52:15 +0200

    added migrations + product model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    CATEGORY_PHONE = "phone"
+    CATEGORY_LAPTOP = "laptop"
+    CATEGORY_TABLET = "tablet"
+
+    CATEGORY_CHOICES = (
+        (CATEGORY_PHONE, "phone"),  # (DB_CODE, "human_readable_name")
+        (CATEGORY_LAPTOP, "laptop"),
+        (CATEGORY_TABLET, "tablet")
+    )
+
+    title = models.CharField(max_length=256)
+    price = models.DecimalField(decimal_places=10, max_digits=20)
+    description = models.TextField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=256)
+
+    def __str__(self):
+        return f"{self.title} : {self.id}"
