eee9bffa661c9cc7f733aa7259df954ee4d446cf
David Simek <david.simek1998@gmail.com>
2022-08-20 14:51:03 +0200

    added base html to reviews

--- shop/forms.py
+++ shop/forms.py
@@ -5,7 +5,7 @@ from shop.models import Product
 
 
 class ProductReviewForm(forms.Form):
-    product = forms.ModelChoiceField(queryset=Product.objects.all(), disabled=True)
-    score = forms.IntegerField()
-    user = forms.ModelChoiceField(queryset=get_user_model().objects.all(), disabled=True)
+    product = forms.ModelChoiceField(queryset=Product.objects.all())
+    score = forms.IntegerField(min_value=0, max_value=10)
+    user = forms.ModelChoiceField(queryset=get_user_model().objects.all())
     text = forms.CharField(widget=forms.Textarea)
--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import redirect, get_object_or_404
 from django.template.response import TemplateResponse
-from shop.models import Product, Cart
+from shop.models import Product, Cart, ProductReview
 from django.contrib.auth import get_user_model
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
 from shop.forms import ProductReviewForm
 
 
@@ -79,17 +79,48 @@ def cart_view(request, pk):
     return TemplateResponse(request, "cart.html", context=context)
 
 
-class ListProductReviewView(TemplateView):
+class ListProductReviewView(FormView):
     template_name = "product_reviews.html"
+    form_class = ProductReviewForm
+
+    def get_initial(self):
+        product = self.get_object()
+        user = get_user_model().objects.first()
+        return {"product": product,  "user": user}
+
+    def get_object(self):
+        return get_object_or_404(Product, pk=self.kwargs["product_pk"])
 
     def get_context_data(self, **kwargs):
         context = super(ListProductReviewView, self).get_context_data(**kwargs)
-
-        product = get_object_or_404(Product, pk=kwargs["product_pk"])
+        product = self.get_object()
         user = get_user_model().objects.first()
-
         context.update({
-            "form": ProductReviewForm(),
-            "product": product
+            "product": product,
+            "cart": user.cart
         })
         return context
+
+    def post(self, request, *args, **kwargs):
+        product = self.get_object()
+        user = get_user_model().objects.first()
+
+        form_data = {
+            "user": user.pk,
+            "product": product.pk,
+            "text": request.POST.get("text"),
+            "score": request.POST.get("score")
+        }
+
+        bounded_form = ProductReviewForm(data=form_data)
+        if not bounded_form.is_valid():
+            return JsonResponse(status=400, data={"message": "invalid_data"})
+
+        ProductReview.objects.create(
+            user=bounded_form.cleaned_data["user"],
+            product=bounded_form.cleaned_data["product"],
+            text=bounded_form.cleaned_data["text"],
+            score=bounded_form.cleaned_data["score"]
+        )
+
+        return self.get(request, *args, **kwargs)
